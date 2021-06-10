c9da46f7ecc9c19bf840a30f1bdff6f212b1ed3b
Milxnor <84999745+Milxnor@users.noreply.github.com>
2021-06-10 18:51:07 -0400

    Add files via upload

---
+++ FPSBooster/FPSBooster/Utils/FortniteBoost.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FPSBooster.Utils
+{
+    /*
+    [1] Windows FPS Boost
+    [2] Config FPS Boost
+    [3] Hardware FPS Boost
+    [4] Other
+    */
+    class FortniteBoost
+    {
+    }
+    class WindowsFN
+    { 
+    }
+    class ConfigFN
+    {
+
+    }
+    class HardwareFN
+    {
+
+    }
+    class OtherFN
+    {
+
+    }
+}
---
+++ FPSBooster/FPSBooster/Utils/vars.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FPSBooster.Utils
+{
+    class vars
+    {
+        public static string localAD = Environment.GetEnvironmentVariable("LocalAppData");
+    }
+}
