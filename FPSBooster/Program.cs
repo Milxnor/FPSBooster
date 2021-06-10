a9150ca15073c4e5952b39aa308bcb336be8d6f7
Milxnor <84999745+Milxnor@users.noreply.github.com>
2021-06-10 18:51:26 -0400

    Add files via upload

---
+++ FPSBooster/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FPSBooster
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(@"
+ ________ ________  ________           ________  ________  ________  ________  _________  _______   ________     
+|\  _____\\   __  \|\   ____\         |\   __  \|\   __  \|\   __  \|\   ____\|\___   ___\\  ___ \ |\   __  \    
+\ \  \__/\ \  \|\  \ \  \___|_        \ \  \|\ /\ \  \|\  \ \  \|\  \ \  \___|\|___ \  \_\ \   __/|\ \  \|\  \   
+ \ \   __\\ \   ____\ \_____  \        \ \   __  \ \  \\\  \ \  \\\  \ \_____  \   \ \  \ \ \  \_|/_\ \   _  _\  
+  \ \  \_| \ \  \___|\|____|\  \        \ \  \|\  \ \  \\\  \ \  \\\  \|____|\  \   \ \  \ \ \  \_|\ \ \  \\  \| 
+   \ \__\   \ \__\     ____\_\  \        \ \_______\ \_______\ \_______\____\_\  \   \ \__\ \ \_______\ \__\\ _\ 
+    \|__|    \|__|    |\_________\        \|_______|\|_______|\|_______|\_________\   \|__|  \|_______|\|__|\|__|
+                      \|_________|                                     \|_________|
+                ");
+            Console.WriteLine(@"
+[1] Windows FPS Boost
+[2] Config FPS Boost
+[3] Hardware FPS Boost
+[4] Other" + "\n");
+            Console.WriteLine("Please pick a number: ");
+            string num = Console.ReadLine();
+            if(num == "1") 
+            Console.ReadLine();
+        }
+    }
+}
